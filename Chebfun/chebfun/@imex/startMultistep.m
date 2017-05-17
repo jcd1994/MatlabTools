ed240f80207e6e969ae47ca4b41ed49232f92f88
Jon <jon-doucette@hotmail.com>
2017-05-17 09:48:50 -0700

    Removed chebfun (to be re-added as submodule)

--- Chebfun/chebfun/@imex/startMultistep.m
+++
@@ -1,7 +0,0 @@
-function [uSol, NuSol] = startMultistep(K, dt, L, Nc, Nv, pref, S, uInit, NuInit)
-%STARTMULTISTEP   Get enough initial data when using a multistep scheme.
-
-% Not implemented of IMEX schemes yet.
-error('Multistep IMEX schemes have not been implemented yet.')
-
-end
